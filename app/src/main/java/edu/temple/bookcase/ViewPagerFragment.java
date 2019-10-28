8de374d4bfc054cccc5461f1e53bf76aafcd064b
Dishant Patel <tug02945@temple.edu>
2019-10-28 18:03:08 -0400

    Built BookDetailsFragment

--- app/src/main/java/edu/temple/bookcase/ViewPagerFragment.java
+++ app/src/main/java/edu/temple/bookcase/ViewPagerFragment.java
@@ -7,30 +7,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link BookDetailsFragment.OnFragmentInteractionListener} interface
+ * {@link ViewPagerFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link BookDetailsFragment#newInstance} factory method to
+ * Use the {@link ViewPagerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BookDetailsFragment extends Fragment {
+public class ViewPagerFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "bookname";
+    private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-
+    private String mParam1;
+    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-    private String bookName;
-    private TextView text;
-    public BookDetailsFragment() {
+
+    public ViewPagerFragment() {
         // Required empty public constructor
     }
 
@@ -38,13 +37,16 @@ public class BookDetailsFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @return A new instance of fragment BookDetailsFragment.
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ViewPagerFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static BookDetailsFragment newInstance(String bookName) {
-        BookDetailsFragment fragment = new BookDetailsFragment();
+    public static ViewPagerFragment newInstance(String param1, String param2) {
+        ViewPagerFragment fragment = new ViewPagerFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, bookName);
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -53,20 +55,24 @@ public class BookDetailsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-           this.bookName = getArguments().getString(ARG_PARAM1);
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_book_details, container, false);
-        text = v.findViewById(R.id.textView);
-        text.setText(this.bookName);
-        return v;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_pager, container, false);
     }
 
-
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -97,6 +103,6 @@ public class BookDetailsFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        
+        void onFragmentInteraction(Uri uri);
     }
 }
