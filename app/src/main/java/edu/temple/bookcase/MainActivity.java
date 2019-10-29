277ad4839dccb010af54ab6aaeb7a8a4047d4ff0
Dishant Patel <tug02945@temple.edu>
2019-10-29 16:21:21 -0400

    Added background strings and such

--- app/src/main/java/edu/temple/bookcase/MainActivity.java
+++ app/src/main/java/edu/temple/bookcase/MainActivity.java
@@ -1,15 +1,29 @@
 package edu.temple.bookcase;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean twoPane = false;
-
+    private boolean twoPane = false;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        twoPane = findViewById(R.id.container2) == null;
+        Context context = getApplicationContext();
+        Resources res = context.getResources();
+        final String names[] = res.getStringArray(R.array.bookNames);
+
+        if(!twoPane){
+            
+        }
+
     }
 }
